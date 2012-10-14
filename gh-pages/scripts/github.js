e38abc039c0d38692c48d525fa1070fea485e7db
Bart Riemens <briemens@crafity.com>
2012-10-14 03:59:18 +0200

    Changed the sort order to size and added size and forked to the details

--- gh-pages/scripts/github.js
+++ gh-pages/scripts/github.js
@@ -6,6 +6,7 @@ $(function () {
 		, loadingMessage = section.find('.loading.message')
 		, loadingCount = 0
 		, markdown = new Showdown.converter();
+	$(".repository.template").detach();
 	
 	getUserInfo(Gh3, "Crafity");
 	section.delegate("a.show.readme", "click", function () {
@@ -67,15 +68,31 @@ $(function () {
 			loadingMessage.remove();
 			loadingMessage = undefined;
 		}
+		var lastUpdated = new Date(repo.updated_at);
+		lastUpdated = "" + lastUpdated.getDate() + "-" + (lastUpdated.getMonth() + 1) + "-" + lastUpdated.getFullYear();
 		var instance = template
 			.replace(/{{name}}/gmi, repo.name)
 			.replace(/{{description}}/gmi, repo.description)
 			.replace(/{{watchers_count}}/gmi, repo.watchers_count)
 			.replace(/{{open_issues_count}}/gmi, repo.open_issues_count)
-			.replace(/{{updated_at}}/gmi, repo.updated_at)
+			.replace(/{{size}}/gmi, repo.size)
+			.replace(/{{show_fork}}/gmi, repo.fork ? "" : "display:none")
+			.replace(/{{updated_at}}/gmi, lastUpdated)
 			.replace(/{{readme}}/gmi, markdown.makeHtml(readme))
 			.replace(/readme hidden/gmi, "readme " + (readme ? "" : "hidden"));
-		section.append(instance);
+//		console.log("repo.open_issues", repo.open_issues);
+		$("section .repositories").children().each(function () {
+			var this$ = $(this);
+//			console.log(this$.find(".size").text());
+			var size = parseInt(this$.find(".size").text(), 10);
+			if (!instance || parseInt(repo.size, 10) < size) {
+//				console.log(parseInt(repo.size, 10), size);
+			} else {
+				$(instance).insertBefore(this$);
+				instance = undefined;
+			}
+		});
+		instance && section.append(instance);
 	}
 });
 	
